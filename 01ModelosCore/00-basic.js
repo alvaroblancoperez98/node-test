f2ecc98c9f71e81710d47d96d8ed97d21f4d2181
alvaroblancoperez98 <114095624+alvaroblancoperez98@users.noreply.github.com>
2023-01-16 19:38:51 +0100

    Add files via upload

---
+++ 01ModelosCore/00-basic.js
@@ -0,0 +1,22 @@
+<<<<<<< HEAD
+/*
+Esto es un primer ejemplo de un archivo
+ejecutado desde node
+*/
+console.log('Hola Mundo desde Node.js, esto se verá en el terminal de comandos')
+console.log(2 + 5)
+console.log(DAW)
+setInterval(function (){
+   console.log('Hola Node.js')
+=======
+/*
+Esto es un primer ejemplo de un archivo
+ejecutado desde node
+*/
+console.log('Hola Mundo desde Node.js, esto se verá en el terminal de comandos')
+console.log(2 + 5)
+console.log(DAW)
+setInterval(function (){
+   console.log('Hola Node.js')
+>>>>>>> 0eaba20a44c3d1461484c90785ba0bcebdefa246
+}, 1000)
---
+++ 01ModelosCore/07-event-inherit.js
@@ -0,0 +1,25 @@
+var EventEmitter = require('events').EventEmitter;
+class Clock extends EventEmitter {
+
+    constructor(){
+        super();
+        setInterval(() => {
+            this.emit('tictac')
+        }, 1000);
+    }
+
+    theTime() {
+        var date = new Date(),
+        hrs = date.getHours(),
+        min = date.getMinutes(),
+        sec = date.getSeconds(),
+        msg = hrs + ':' + min + ':' + sec;
+
+        console.log(msg);
+    }
+}
+
+var tula = new Clock()
+tula.on('tictac', function(){
+    tula.theTime()
+})
