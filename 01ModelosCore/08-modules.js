be58f8d0e8ff941979bf90fb184bc4069ec27e1a
alvaroblancoperez98 <114095624+alvaroblancoperez98@users.noreply.github.com>
2023-02-06 17:59:41 +0100

    Delete 01ModelosCore directory

--- 01ModelosCore/08-modules.js
+++
@@ -1,7 +0,0 @@
-var myData = require('./my-data')//No es necesaria la extensión .js/.json
-
-console.log(
-    myData.nombre,
-    myData.correo,
-    myData.telefono
-)
--- 01ModelosCore/main.js
+++
@@ -1,7 +0,0 @@
-var reloj = require("./reloj");
-
-var miReloj = new reloj();
-
-miReloj.on("tictac", ()=>{
-    miReloj.theTime();
-});
